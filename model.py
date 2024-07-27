3447bebeb05f47099142a425ffa2f63c8fd23f16
loc-vu <vhploc@gmail.com>
2024-07-26 17:42:57 -0700

    completed first version, Andrej Karpathy tutorial

--- model.py
+++ model.py
@@ -2,6 +2,12 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+learning_rate = 3e-4
+max_iters = 5000
+eval_interval = 500
+eval_iter = 200
+
+batch_size = 64
 block_size = 256
 
 dropout = 0.2
@@ -12,6 +18,44 @@ n_layer = 6
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 vocab_size = 56
 
+with open('./data/input.txt', 'r') as f:
+    text = f.read()
+
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+
+stoi = { c:i for i, c in enumerate(chars)}
+itos = { i:c for i, c in enumerate(chars)}
+encode = lambda s: [ stoi[c] for c in s]
+decode = lambda l: "".join([ itos[i] for i in l])
+
+data = torch.tensor(encode(text), dtype=torch.long)
+n_train = int(0.9*len(data))
+train_data = data[:n_train]
+val_data = data[n_train:]
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x,y = x.to(device), y.to(device)
+
+    return x, y
+
+@torch.no_grad
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iter)
+        for k in range(eval_iter):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out['split'] = losses.mean()
+    model.train()
+    return out
 
 class Head(nn.Module):
     def __init__(self, head_size):
@@ -124,3 +168,25 @@ class GPTLanguageModel(nn.Module):
             idx = torch.concat((idx, idx_next), dim=1)
         
         return idx
+    
+model = GPTLanguageModel()
+m = model.to(device)
+
+print(f"Number of parameter in model: {sum(p.numel() for p in m.parameters())/1e6}M parameters")
+
+optimizer = torch.optim.AdamW(model.paramters(), lr=learning_rate)
+
+for iter in range(max_iters):
+    if iter % eval_interval == 0 or iter == max_iters - 1:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+    xb, yb = get_batch('train')
+
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
